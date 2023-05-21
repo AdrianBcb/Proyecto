652b59ea2c007ed056e1308babc8bf03f147671c
Tredexi <nathadiel4@gmail.com>
2023-05-21 01:45:43 -0500

    complete estas clases, y modifique con base se necesita

--- ProyectoFinal/src/clases/alumnos.java
+++ ProyectoFinal/src/clases/alumnos.java
@@ -1,5 +1,33 @@
 package clases;
 
 public class alumnos extends Persona {
+String Tutor;
+String Telefono;
+public String Direccion;
+public alumnos(String nombre, String apellidos, String sexo, String nacionalidad, String tutor, String telefono,
+		String direccion) {
+	super(nombre, apellidos, sexo, nacionalidad);
+	Tutor = tutor;
+	Telefono = telefono;
+	Direccion = direccion;
+}
+public String getTutor() {
+	return Tutor;
+}
+public void setTutor(String tutor) {
+	Tutor = tutor;
+}
+public String getTelefono() {
+	return Telefono;
+}
+public void setTelefono(String telefono) {
+	Telefono = telefono;
+}
+public String getDireccion() {
+	return Direccion;
+}
+public void setDireccion(String direccion) {
+	Direccion = direccion;
+}
 
 }
--- ProyectoFinal/src/clases/profesores.java
+++ ProyectoFinal/src/clases/profesores.java
@@ -1,5 +1,25 @@
 package clases;
 
 public class profesores extends Persona {
-
+	public String AñoNac;
+	public String Asignatura;
+	public profesores(String nombre, String apellidos, String sexo, String nacionalidad, String añoNac,
+			String asignatura) {
+		super(nombre, apellidos, sexo, nacionalidad);
+		AñoNac = añoNac;
+		Asignatura = asignatura;
+	}
+	public String getAñoNac() {
+		return AñoNac;
+	}
+	public void setAñoNac(String añoNac) {
+		AñoNac = añoNac;
+	}
+	public String getAsignatura() {
+		return Asignatura;
+	}
+	public void setAsignatura(String asignatura) {
+		Asignatura = asignatura;
+	}
+	
 }
