b4ab5c81ca80468caac633a8b99d587f9ce36748
Adrian Bacab <bacabadrian7@gmail.com>
2023-05-21 20:18:53 -0500

    funcionando

--- ProyectoFinal/src/GUI/Registro.java
+++ ProyectoFinal/src/GUI/Registro.java
@@ -6,6 +6,8 @@ import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 
@@ -42,76 +44,84 @@ public class Registro extends JPanel {
 		
 		JLabel lblnombre = new JLabel("Nombre");
 		lblnombre.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblnombre.setBounds(92, 55, 107, 17);
+		lblnombre.setBounds(105, 54, 107, 17);
 		panel_1.add(lblnombre);
 		
 		JLabel lblapellidos = new JLabel("Apellidos");
 		lblapellidos.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblapellidos.setBounds(92, 94, 96, 17);
+		lblapellidos.setBounds(105, 94, 96, 17);
 		panel_1.add(lblapellidos);
 		
 		JLabel lblsexo = new JLabel("Sexo");
 		lblsexo.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblsexo.setBounds(92, 138, 96, 17);
+		lblsexo.setBounds(105, 138, 96, 17);
 		panel_1.add(lblsexo);
 		
 		JLabel lblnacionalidad = new JLabel("Nacionalidad");
 		lblnacionalidad.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblnacionalidad.setBounds(92, 176, 96, 17);
+		lblnacionalidad.setBounds(105, 177, 96, 17);
 		panel_1.add(lblnacionalidad);
 		
 		JLabel lbltutor = new JLabel("Tutor");
 		lbltutor.setFont(new Font("Arial", Font.PLAIN, 14));
-		lbltutor.setBounds(92, 260, 96, 17);
+		lbltutor.setBounds(105, 260, 96, 17);
 		panel_1.add(lbltutor);
 		
 		JLabel lbldireccion = new JLabel("Direccion");
 		lbldireccion.setFont(new Font("Arial", Font.PLAIN, 14));
-		lbldireccion.setBounds(92, 220, 96, 17);
+		lbldireccion.setBounds(105, 220, 96, 17);
 		panel_1.add(lbldireccion);
 		
 		JLabel lbltelefono = new JLabel("Telefono");
 		lbltelefono.setFont(new Font("Arial", Font.PLAIN, 14));
-		lbltelefono.setBounds(92, 299, 96, 17);
+		lbltelefono.setBounds(105, 299, 96, 17);
 		panel_1.add(lbltelefono);
 		
 
 		JComboBox comboBox_na = new JComboBox();
-		comboBox_na.setModel(new DefaultComboBoxModel(new String[] {"Mexicana"}));
+		comboBox_na.setModel(new DefaultComboBoxModel(new String[] {"Mexicana", "Extranjera"}));
 		comboBox_na.setFont(new Font("Arial", Font.PLAIN, 14));
-		comboBox_na.setBounds(242, 175, 153, 20);
+		comboBox_na.setBounds(262, 175, 153, 20);
 		panel_1.add(comboBox_na);
 		
 		JComboBox comboBox_se = new JComboBox();
 		comboBox_se.setToolTipText("");
-		comboBox_se.setModel(new DefaultComboBoxModel(new String[] {"H", "M"}));
+		comboBox_se.setModel(new DefaultComboBoxModel(new String[] {"H", "M",}));
 		comboBox_se.setFont(new Font("Arial", Font.PLAIN, 14));
-		comboBox_se.setBounds(242, 135, 153, 20);
+		comboBox_se.setBounds(262, 136, 153, 20);
 		panel_1.add(comboBox_se);
+		
+		JComboBox comboBox_A = new JComboBox();
+		comboBox_A.setToolTipText("");
+		comboBox_A.setModel(new DefaultComboBoxModel(new String[] {"Literatura", "Kun Fu","Karate-do"}));
+		comboBox_A.setFont(new Font("Arial", Font.PLAIN, 14));
+		comboBox_A.setBounds(262, 340, 153, 20);
+		panel_1.add(comboBox_A);
 
 		textField_nombre = new JTextField();
+		textField_nombre.setFont(new Font("Arial", Font.PLAIN, 14));
 		textField_nombre.setColumns(10);
-		textField_nombre.setBounds(241, 55, 141, 19);
+		textField_nombre.setBounds(262, 53, 141, 19);
 		panel_1.add(textField_nombre);
 		
 		textField_apelllidos = new JTextField();
 		textField_apelllidos.setColumns(10);
-		textField_apelllidos.setBounds(241, 94, 141, 19);
+		textField_apelllidos.setBounds(262, 94, 141, 19);
 		panel_1.add(textField_apelllidos);
 		
 		textField_direccion = new JTextField();
 		textField_direccion.setColumns(10);
-		textField_direccion.setBounds(241, 220, 141, 19);
+		textField_direccion.setBounds(262, 220, 141, 19);
 		panel_1.add(textField_direccion);
 		
 		textField_tutor = new JTextField();
 		textField_tutor.setColumns(10);
-		textField_tutor.setBounds(241, 260, 141, 19);
+		textField_tutor.setBounds(262, 260, 141, 19);
 		panel_1.add(textField_tutor);
 		
 		textField_telefono = new JTextField();
 		textField_telefono.setColumns(10);
-		textField_telefono.setBounds(241, 299, 141, 19);
+		textField_telefono.setBounds(262, 299, 141, 19);
 		panel_1.add(textField_telefono);
 
 		
@@ -122,33 +132,40 @@ public class Registro extends JPanel {
 				String nombre=textField_nombre.getText();
 				String apellidos=textField_apelllidos.getText();
 				String direccion=textField_direccion.getText();
-				String sexo=String.valueOf(comboBox_se.getSelectedIndex());
-				String nacionalidad=String.valueOf(comboBox_na.getSelectedIndex());
+				
+				String sexo=String.valueOf(comboBox_se.getSelectedItem());
+				String nacionalidad=String.valueOf(comboBox_na.getSelectedItem());
+				String asignatura=String.valueOf(comboBox_A.getSelectedItem());
 				String tutor=textField_tutor.getText();
 				String telefono=textField_telefono.getText();
-				m.registrar(nombre, apellidos, direccion, sexo, nacionalidad, tutor, telefono);
-				textField_nombre.setText("");
-				textField_apelllidos.setText("");
-				textField_direccion.setText("");
-				textField_tutor.setText("");
-				textField_telefono.setText("");
-				
-				//	alumnos alumnoR = new Persona(textField_nombre.getText(), textField_apelllidos.getText(),
-				//		textField_direccion.getText(), textField_sexo.getText(), ABORTa,textField.naciona TOOL_TIP_TEXT_KEY);
-			//	m.registrar(alumnoR);
-				
-				
+				boolean z = m.registrar(nombre, apellidos, direccion, sexo, nacionalidad, tutor, telefono, asignatura);
+				if (z==true) {
+					textField_nombre.setText("");
+					textField_apelllidos.setText("");
+					textField_direccion.setText("");
+					textField_tutor.setText("");
+					textField_telefono.setText("");
+					JOptionPane.showMessageDialog(btn_registrar, "Alumno registrado correctamente");
+				}else {
+					JOptionPane.showMessageDialog(btn_registrar, "No se pudo registrar");
+
+				}
 			}
 		});
 		btn_registrar.setFont(new Font("Arial", Font.BOLD, 16));
-		btn_registrar.setBounds(174, 360, 129, 27);
+		btn_registrar.setBounds(183, 390, 129, 27);
 		panel_1.add(btn_registrar);
+		
+		JLabel lblAsignatura = new JLabel("Asignatura");
+		lblAsignatura.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblAsignatura.setBounds(105, 340, 96, 19);
+		panel_1.add(lblAsignatura);
 		
 		JLabel lbl_fondo = new JLabel("");
 		lbl_fondo.setBounds(0, 0, 546, 427);
 		lbl_fondo.setIcon(new ImageIcon("img/fondo2.jpg"));
-
-		panel_1.add(lbl_fondo);
+		
+				panel_1.add(lbl_fondo);
 		
 	}
 }
--- ProyectoFinal/src/mysql/metodos.java
+++ ProyectoFinal/src/mysql/metodos.java
@@ -3,23 +3,26 @@ import java.sql.*;
 import clases.alumnos;
 
 public class metodos {
+	
+	
+	
 	static String url1 = "jdbc:mysql://127.0.0.1:3306/escuela";
     static String user = "root";
-    static  String password = "Tredex$03";
-	public boolean registrar(String nombre, String apellidos,String direccion,String sexo,String nacionalidad,String tutor, String telefono) {
+    static  String password = "05Lanabe";
+    
+    
+    
+	public boolean registrar(String nombre, String apellidos,String direccion,String sexo,String nacionalidad,String tutor, String telefono, String a) {
 		try { 
             Connection MyConn = DriverManager.getConnection(url1, user, password);
 		    Statement myStmt  = MyConn.createStatement();
-		    //String sql = "INSERT INTO alumnos(nombre, apellidos, direccion, sexo, nacionalidad, tutor, telefono tutor)"+"VALUES ('"+nombre+"','"+apellidos+"', '"+direccion+"', '"+sexo+"', '"+tutor+"', '"+telefono+"','"+ nacionalidad+"')";
-		   // INSERT INTO `escuela`.`alumnos` (`idAlumnos`, `Nombre`, `Apellidos`, `Direccion`, `Sexo`, `Nacionalidad`, `Tutor`, `Telefono tutor`) VALUES ('51', 'D', 'd', 'f', 'f', 'f', 't', 'my');
-		   // String sql="Insert into alumnos(Nombre,Apellidos,Direccion,Sexo,Nacionalidad,Tutor,Telefono tutor)"+"values('"+nombre+"','"+apellidos+"','"+sexo+"','"+nacionalidad+"','"+tutor+"','"+direccion+"','"+telefono+"')";
-		   // String Registro="INSERT INTO alumnos(Nombre,Apellidos,Direccion,Sexo,Nacionalidad,Tutor,Telefono tutor)"+"VALUES('"+nombre+"','"+apellidos+"','"+direccion+"','"+
-		   //sexo+"','"+nacionalidad+"','"+tutor+"','"+telefono+"')"; 
-		    String sql="Insert into alumnos"
-		    		+"(nombre, apellidos, direccion, sexo, nacionalidad, tutor, telefono tutor) "
-		    		+ "values('"+nombre+"','"+apellidos+"','"+direccion+"','"+sexo+"','"+nacionalidad+"','"+tutor+"','"+telefono+"')";
-		    System.out.println(sql);
-		    myStmt.executeUpdate(sql);
+		   
+		    String ejecutar="INSERT INTO alumnos "+"(Nombre, Apellidos, Direccion, Sexo, Nacionalidad, Tutor, Telefono_tutor)"+" values ("
+		    										+"'"+nombre+"'"+","+"'"+apellidos+"'"+","+"'"+direccion+"'"+","+"'"+sexo+"'"+","+"'"+nacionalidad+"'"+","+"'"+tutor+"'"+","+"'"+telefono+"'"+"'"+a+"'"+");";
+
+		    //String ejecutar="INSERT INTO alumnos (" +nombre+ ","+ apellidos+","+direccion+","+sexo+","+nacionalidad+","+tutor+","+telefono+");";
+		    //System.out.println(ejecutar);
+		    myStmt.executeUpdate(ejecutar);
 		    return true;
 	     }
 		catch (Exception e) {
@@ -27,15 +30,41 @@ public class metodos {
 			return false;
 		}
 	}
+	
+	
+	
+	
+	
 	public alumnos buscar(String busca) {
 	
 	alumnos a = new alumnos(busca, busca, busca, busca, busca, busca, busca);
 	return a;
 	//lo mismo pero mas chido a prueba de errores
 }
+	
+	
+	
+	
+	
+	
+	
+	
 	public void eliminar(alumnos a) {
 	//este yo lo hago
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public void editar(alumnos a) {
 	//este surgió solito
 }
